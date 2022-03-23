be82aa6100e8a02394e45833e9675e6ca28246e0
elizabethwyatt <lizzyawyatt@gmail.com>
2022-03-23 13:51:08 -0400

    add start and stop receiving logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, VERSION } from '@angular/core';
 import { interval } from 'rxjs';
+import { takeWhile } from 'rxjs/operators';
 
 @Component({
   selector: 'my-app',
@@ -7,9 +8,21 @@ import { interval } from 'rxjs';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  name = 'Angular ' + VERSION.major;
+  count = 1;
+  receiving = false;
 
-  source = interval(300);
-  example = this.source.pipe();
-  subscribe = this.example.subscribe((val) => console.log(val));
+  start = () => {
+    this.receiving = true;
+    interval(300)
+      .pipe(takeWhile(() => this.receiving))
+      .subscribe(() => this.count++);
+  };
+
+  stop = () => {
+    this.receiving = false;
+  };
+
+  reset = () => {
+    this.count = 0;
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FlexLayoutModule } from '@angular/flex-layout';
-//import { FormsModule } from '@angular/forms';
 import { MatBadgeModule } from '@angular/material/badge';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
@@ -13,7 +12,6 @@ import { HelloComponent } from './hello.component';
   imports: [
     BrowserModule,
     FlexLayoutModule,
-    //FormsModule,
     MatBadgeModule,
     MatButtonModule,
     MatIconModule,
